03ff683816bfc8e4c8c0b2c19fd0227b92656754
endeav0r <endeavor@rainbowsandpwnies.com>
2011-09-18 17:14:03 -0400

    changing got_offset_find.lua to work with simplified tables produced by lua.c

--- tools/got_offset_find.lua
+++ tools/got_offset_find.lua
@@ -22,35 +22,37 @@ sublog = elf_open(TARGET_FILENAME)
 
 -- find all symbols that are GLIBC in TARGET
 glibc_symbols = {}
-for k, sec in pairs(sublog["sections"]) do
+for sec_name, sec in pairs(sublog["sections"]) do
     if sec["type"] == "symtab" then
-        for i, symbol in pairs(sec["symbols"]) do
-            if string.find(symbol["name"], "@@GLIBC") then
-                glibc_symbols[symbol["name"]] = symbol
+        for name, symbol in pairs(sec["symbols"]) do
+            if string.find(name, "@@GLIBC") then
+                glibc_symbols[name] = symbol
             end
         end
     end
 end
 
 -- cut out the @@GLIBC part
+tmp = {}
 for name, symbol in pairs(glibc_symbols) do
     offset = string.find(name, "@@GLIBC")
-    symbol["name"] = string.sub(symbol["name"], 0, offset - 1)
+    tmp[string.sub(name, 0, offset - 1)] = symbol
 end
+glibc_symbols = tmp
 
 -- print out our symbols we're looking for
 for name, symbol in pairs(glibc_symbols) do
-    print("looking for " .. symbol["name"])
+    print("looking for " .. name)
 end
 
 -- go ahead and search through libc symbols and pull out symbols that match
 libc_symbols = {}
-for k, sec in pairs(libc["sections"]) do
+for name, sec in pairs(libc["sections"]) do
     if sec["type"] == "dynsym" then
-        for i, symbol in pairs(sec["symbols"]) do
-            for j, glibc_symbol in pairs(glibc_symbols) do
-                if symbol["name"] == glibc_symbol["name"] then
-                    libc_symbols[symbol["name"]] = symbol
+        for libc_name, symbol in pairs(sec["symbols"]) do
+            for glibc_name, glibc_symbol in pairs(glibc_symbols) do
+                if libc_name == glibc_name then
+                    libc_symbols[libc_name] = symbol
                 end
             end
         end
@@ -70,7 +72,7 @@ print("finding rops within offset")
 valid_rops = {}
 -- find all rops that are within ROP_OFFSET_SIZE from the addresses in libc_symbols
 for k, rop in pairs(rop_table) do
-    for k, libc_symbol in pairs(libc_symbols) do
+    for name, libc_symbol in pairs(libc_symbols) do
         if not rop_added then
             if rop[1]["address"] < libc_symbol["address"] + ROP_OFFSET_SIZE and 
                rop[1]["address"] > libc_symbol["address"] - ROP_OFFSET_SIZE then
@@ -81,7 +83,7 @@ for k, rop in pairs(rop_table) do
                 else
                     sign = "+"
                 end
-                valid_rops[libc_symbol["name"] .. " " .. sign .. w2h(offset)] = rop
+                valid_rops[name .. " " .. sign .. tostring(offset)] = rop
                 break
             end
         end
