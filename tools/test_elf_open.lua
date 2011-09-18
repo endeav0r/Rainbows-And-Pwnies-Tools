4b9da7f4bcd533e39b70988fb93b23475d7eb168
endeav0r <endeavor@rainbowsandpwnies.com>
2011-09-18 17:13:22 -0400

    reworking test_elf_open.lua to work with simplified tables produced by lua.c

--- tools/test_elf_open.lua
+++ tools/test_elf_open.lua
@@ -3,15 +3,13 @@ dofile("tools/rop_tools.lua")
 --[[
 ["shnum"] integer
 ["sections"] table
-    [section_i] integer
-        ["name"] string
+    ["name" or index if name is NULL] string
         ["address"] integer
         ["executable"] boolean
         ["size"] integer
         ["type"] string
         ["symbols"] table
-            [symbol_i] integer
-                ["name"] string
+            ["name" or index if name is NULL] integer
                 ["address"] integer
                 ["type"] string
 ]]
@@ -21,17 +19,23 @@ elf = elf_open("rop_tools")
 print("shnum " .. tostring(elf["shnum"]))
 
 -- go through each section
-for k, section in pairs(elf["sections"]) do
-    print("  name:    " .. section["name"])
+for name, section in pairs(elf["sections"]) do
+    print(name)
     print("  address: " .. w2h(section["address"]))
     print("  size:    " .. tostring(section["size"]))
     print("  type:    " .. section["type"])
-    -- print symbols
+    
+    if section["type"] == "rel" then
+        for name, rel in pairs(section["relocations"]) do
+            print("    " .. name .. " => " .. w2h(rel["offset"]))
+        end
+    end
+    
     if section["symbols"] ~= nil then
-        for k, symbol in pairs(section["symbols"]) do
-            print("    " .. tostring(k) .. " name:    " .. symbol["name"])
-            print("    " .. tostring(k) .. " address: " .. w2h(symbol["address"]))
-            print("    " .. tostring(k) .. " type:    " .. symbol["type"])
+        for name, symbol in pairs(section["symbols"]) do
+            print("    " .. name)
+            print("      address: " .. w2h(symbol["address"]))
+            print("      type:    " .. symbol["type"])
         end
     end
 end
