3723bfd169ea4e067651026ab6493d43f4a5e237
endeav0r <endeavor@rainbowsandpwnies.com>
2011-10-11 09:08:30 -0400

    fixing bug in elfdis.lua that caused fatal error if a jmp was encountered which could not resolve to an address

--- tools/elfdis.lua
+++ tools/elfdis.lua
@@ -136,14 +136,13 @@ function print_instructions (elf, instructions)
     local jump_destinations = {}
     local jump_origins = {}
     for index, instruction in pairs(instructions) do
+        abs = operand_abs(instruction["operands"][1],
+                          instruction["address"], instruction["size"])
         if is_jump(instruction["mnemonic"]) and 
-           operand_abs(instruction["operands"][1],
-                       instruction["address"],
-                       instruction["size"]) >= instructions[1]["address"] and
-           operand_abs(instruction["operands"][1],
-                       instruction["address"],
-                       instruction["size"]) <= instructions[#instructions]["address"] +
-                        uint_t.new(32, instructions[#instructions]["size"]) then
+                   abs and
+                   abs >= instructions[1]["address"] and
+                   abs <= instructions[#instructions]["address"]
+                          + uint_t.new(32, instructions[#instructions]["size"]) then
             table.insert(jump_origins, instruction["address"])
             table.insert(jump_destinations, (instruction["address"]:int_t() + 
                                              instruction["operands"][1]["lval"] +
