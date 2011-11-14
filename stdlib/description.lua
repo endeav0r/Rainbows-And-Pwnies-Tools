3e2924bfdd2e5b2f9ed2c1f9f80debdb1138b3a1
endeav0r <endeavor@rainbowsandpwnies.com>
2011-11-14 15:16:04 -0800

    lua operand_abs rewrite, and adding more stdlib files that didn't make it into previous commits

---
+++ stdlib/description.lua
@@ -0,0 +1,24 @@
+function describe_address_symbol (exec, address)
+    for si, s in pairs(exec:symbols()) do
+        if s:address() == address then
+            return 'sym: ' .. s:name()
+        end
+    end
+    return nil
+end
+
+function describe_address_section (exec, address)
+    for si, s in pairs(exec:sections()) do
+        if s:address() <= address and
+           s:address() + uint_t.new(s:address():size(), s:size()) > address then
+            return 'sec: ' .. s:name() .. '+' .. (address - s:address()):strx() .. ' (' .. address:strx() .. ')'
+        end
+    end
+    return nil
+end
+
+function describe_address (exec, address)
+    local description = describe_address_symbol(exec, address)
+    if description ~= nil then return description end
+    return describe_address_section(exec, address)
+end
---
+++ stdlib/disassembly.lua
@@ -0,0 +1,38 @@
+function is_jump (mnemonic)
+    if mnemonic == "jmp" or
+       mnemonic == "jo" or
+       mnemonic == "jno" or
+       mnemonic == "jb" or
+       mnemonic == "jae" or
+       mnemonic == "jz" or
+       mnemonic == "jnz" or
+       mnemonic == "jbe" or
+       mnemonic == "ja" or
+       mnemonic == "js" or
+       mnemonic == "jp" or
+       mnemonic == "jnp" or
+       mnemonic == "jl" or
+       mnemonic == "jge" or
+       mnemonic == "jle" or
+       mnemonic == "jg" then
+        return true
+    end
+    return false
+end
+
+
+function operand_abs (instruction, operand_i)
+    local operand = instruction['operands'][operand_i]
+    local absolute
+    
+    absolute = operand['lval']
+
+    if (operand['type'] == 'mem' and operand['base'] == 'rip')
+       or (operand['type'] == 'jimm') then
+        absolute = absolute + instruction['address']:int_t()
+        absolute = absolute + int_t.new(absolute:size(), instruction['size'])
+    end
+    
+    return absolute
+end
+
